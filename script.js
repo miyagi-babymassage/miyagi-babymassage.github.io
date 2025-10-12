0905ecf0e6f7074becfbbc0779e522c4e1501d0c
宮城ベビーマッサージ <yomi.5183609274@gmail.com>
2025-10-12 17:03:14 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const nav = document.getElementById("nav");
-
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
-});
