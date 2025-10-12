b1c6d7284e30b2f7a92159151be6224d16531a52
宮城ベビーマッサージ <yomi.5183609274@gmail.com>
2025-10-12 16:41:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
   const nav = document.getElementById("nav");
-  const closeBtn = document.getElementById("close");
 
   hamburger.addEventListener("click", () => {
-    nav.classList.add("active");
-  });
-
-  closeBtn.addEventListener("click", () => {
-    nav.classList.remove("active");
+    nav.classList.toggle("active");
   });
 });
